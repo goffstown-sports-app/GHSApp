b8c962c7e783b0a170dd4409688e576386091a5d
Matthew Gleich <matthewgleich@gmail.com>
2020-01-19 19:55:57 -0500

    Use custom theme for colors

--- lib/main.dart
+++ lib/main.dart
@@ -6,10 +6,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: "Goffstown Sport App",
+      theme: ThemeData(primaryColor: Color.fromRGBO(102, 27, 17, 1.0)),
       home: Scaffold(
         appBar: AppBar(
           title: Text("Goffstown Sports"),
-          backgroundColor: Color.fromRGBO(102, 27, 17, 1.0),
         ),
       ),
     );
